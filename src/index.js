8c252a2b572fd95c6b3f7633c5572724efc1e9ac
Павел <70842353+Pashabard19981@users.noreply.github.com>
2022-08-08 19:16:49 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    let nStroka = n.toString();
+    let Array = [...nStroka];
+    let newArray = [];
+     if (n < 0) {
+        let Array2 = Array.shift();
+       for(let i = 0;i < Array.length;i++){
+         newArray.unshift(Array[i]);
+       } return Number(newArray.join(''));
+     } else if (n > 0){
+       for(let i = 0;i < Array.length; i++){
+         newArray.unshift(Array[i]);
+       } return  Number(newArray.join(''));
+     } 
+        
+    
 }
